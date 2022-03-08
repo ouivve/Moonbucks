0523c6603d985a397d0196831d62827a0fcf46ae
ouivve <>
2022-03-08 14:12:51 +0900

    메뉴 추가하기 - 사용자가 입력한 값을 메뉴 리스트에 추가하기 | DOM 핸들링

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,41 @@
+const $ = (selector) => document.querySelector(selector);
+
 function App() {
   // form 태그가 자동으로 전송되는 것을 막음
-  document
-    .querySelector("#espresso-menu-form")
-    .addEventListener("submit", (e) => {
-      e.preventDefault();
-    });
+  $("#espresso-menu-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
 
   // 메뉴 이름 입력 받기
-  document
-    .querySelector("#espresso-menu-name")
-    .addEventListener("keypress", (e) => {
-      if (e.key === "Enter") {
-        console.log(document.querySelector("#espresso-menu-name").value);
-      }
-    });
+  $("#espresso-menu-name").addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      // 메뉴 이름 받아서 보관
+      const espressoMenuName = $("#espresso-menu-name").value;
+
+      // 보관한 값 마크업 추가
+      const menuItemTemplate = (espressoMenuName) => {
+        return `<li class="menu-list-item d-flex items-center py-2">
+          <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+          <button
+            type="button"
+            class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+          >
+            수정
+          </button>
+          <button
+            type="button"
+            class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+          >
+            삭제
+          </button>
+        </li>`;
+      };
+      $("#espresso-menu-list").insertAdjacentHTML(
+        "beforeend",
+        menuItemTemplate(espressoMenuName)
+      );
+    }
+  });
 }
 
 App();
