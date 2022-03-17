ff1c92ffb87c56749a88943ec286318fca734e0a
ouivve <ouivve@gmail.com>
2022-03-17 11:09:26 +0900

    MenuApi 객체 분리 - 메뉴 수정

--- src/js/index.js
+++ src/js/index.js
@@ -20,6 +20,22 @@ const MenuApi = {
       console.error("에러가 발생했습니다. ");
     }
   },
+  async updateMenu(category, name, menuId) {
+    const response = await fetch(
+      `${BASE_URL}/category/${category}/menu/${menuId}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name }),
+      }
+    );
+    if (!response.ok) {
+      console.error("에러가 발생했습니다");
+    }
+    return response.json();
+  },
 };
 
 function App() {
@@ -100,7 +116,9 @@ function App() {
     const template = this.menu[this.currentCategory]
       .map((menuItem, index) => {
         return `
-          <li data-menu-id="${index}" class="menu-list-item d-flex items-center py-2">
+          <li data-menu-id="${
+            menuItem.id
+          }" class="menu-list-item d-flex items-center py-2">
             <span class="w-100 pl-2 menu-name ${
               menuItem.soldOut ? "sold-out" : ""
             }">${menuItem.name}</span>
@@ -158,12 +176,14 @@ function App() {
   };
 
   // 메뉴 이름 수정
-  const updateMenuName = (e) => {
+  const updateMenuName = async (e) => {
     const menuId = e.target.closest("li").dataset.menuId;
     const $menuName = e.target.closest("li").querySelector(".menu-name");
     const updatedMenuName = prompt("메뉴명을 수정하세요.", $menuName.innerText);
-    this.menu[this.currentCategory][menuId].name = updatedMenuName;
-    store.setLocalStorage(this.menu);
+    await MenuApi.updateMenu(this.currentCategory, updatedMenuName, menuId);
+    this.menu[this.currentCategory] = await MenuApi.getAllMenuByCategory(
+      this.currentCategory
+    );
     render();
   };
 
